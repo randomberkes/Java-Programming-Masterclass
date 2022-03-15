e39fb0f7c222ed23c8ae89ae51824cb856bebefc
randomberkes <randomberkes@gmail.com>
2022-03-15 13:29:16 +0100

    Composition lecture

---
+++ Composition/src/com/randomberkes/Dimensions.java
@@ -0,0 +1,25 @@
+package com.randomberkes;
+
+public class Dimensions {
+    private int with;
+    private int height;
+    private int depth;
+
+    public Dimensions(int with, int height, int depth) {
+        this.with = with;
+        this.height = height;
+        this.depth = depth;
+    }
+
+    public int getWith() {
+        return with;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+}
---
+++ Composition/src/com/randomberkes/Motherboard.java
@@ -0,0 +1,41 @@
+package com.randomberkes;
+
+public class Motherboard {
+    private String model;
+    private String manufacturer;
+    private int ramSlot;
+    private int cardSlots;
+    private String bios;
+
+    public Motherboard(String model, String manufacturer, int ramSlot, int cardSlots, String bios) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.ramSlot = ramSlot;
+        this.cardSlots = cardSlots;
+        this.bios = bios;
+    }
+
+    public void loadProgram(String programName){
+        System.out.println("Program " + programName + " is now loading...");
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public int getRamSlot() {
+        return ramSlot;
+    }
+
+    public int getCardSlots() {
+        return cardSlots;
+    }
+
+    public String getBios() {
+        return bios;
+    }
+}
